25ee91c36814a0824a37ab9284df6c0658a464bb
Zhongyi-Li <1404377805@qq.com>
2026-02-09 14:08:57 +0800

    fix: 线上环境删除 dotenv/config 依赖

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import 'dotenv/config';
+if (process.env.NODE_ENV !== 'production') {
+  // Load .env for local development only.
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('dotenv/config');
+}
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
